7eb80c4ca32b6f117be6161f055f6b5566350a28
Amir Shahroudy <amir.shahroudy@zenseact.com>
2024-11-18 22:52:05 +0100

    update io_handler to support both unix and windows platforms

--- python/myutils/io_handler.py
+++ python/myutils/io_handler.py
@@ -9,11 +9,11 @@ AoCData = namedtuple("AoCData", ["year", "day", "input_file"])
 
 
 def get_input_data(filename: str):
-    file_parts = filename.split("/")
+    file_parts = filename.replace('\\','/').split("/")
     day = int(re.search(r"\d+", file_parts[-2]).group(0))
     year = int(re.search(r"\d+", file_parts[-3]).group(0))
 
-    input_file = f'{os.environ.get("aoc_inputs")}/aoc{year}_day{day:02}.txt'
+    input_file = f'{os.getenv("aoc_inputs")}/aoc{year}_day{day:02}.txt'
     if not Path(input_file).exists() or len(Path(input_file).read_text()) == 0:
         Path(input_file).write_text(get_data(day=day, year=year))
     return AoCData(year, day, input_file)
