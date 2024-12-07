3a7ccb7b430f8fcd084ea214aaf386658b167782
Amir Shahroudy <amir.shahroudy@zenseact.com>
2024-12-07 20:00:40 +0100

    move some contents from the template to utils scrip

--- python/myutils/day00/d00.py
+++ python/myutils/day00/d00.py
@@ -12,12 +12,11 @@ import numpy as np
 import scipy as sp
 from myutils.io_handler import get_input_data, submit_answer
 from myutils.search import Search, Search_AStar, Search_BFS, Search_DFS, Search_MinHeap
-from myutils.utils import multiply
+from myutils.utils import *
 from sympy import Symbol
 from sympy.solvers import solve
 
 
-# State = namedtuple("state", ["x","y"])
 class Puzzle:
     def __init__(self, filename):
         self.input_text = Path(filename).read_text()
@@ -34,142 +33,26 @@ class Puzzle:
         self.dir_chars_turn_right = {">": "v", "v": "<", "<": "^", "^": ">"}
         self.dir_chars_turn_reverse = {">": "<", "<": ">", "^": "v", "v": "^"}
 
-        # self.read_ints()
-        # self.read_line_groups()
-        # self.read_int_line_groups()
-        # self.process_int_list()
-        # self.process_int_int_dict()
-        # self.process_int_list_dict()
-        # self.process_map()
+        # read_ints(self)
+        # read_line_groups(self)
+        # read_int_line_groups(self)
+        # process_int_list(self)
+        # process_int_int_dict(self)
+        # process_int_list_dict(self)
+        # process_map(self)
 
-        # self.process()
-        # self.recursive_split("\n")
-        # self.find_all_re()
-        # self.find_all_per_line_re()
+        # process()
+        # recursive_split(self, "\n:", strip=True)
+        # find_all_re()
+        # find_all_per_line_re()
 
-    def process(self):
-        lines = self.input_text.splitlines()
-        self.inp = []
-
-        # self.inp = [tuple(map(int, re.split(r"\D", line))) for line in self.lines]
-
-        # line_re = re.compile(r"(\d+)\s*(\d+):(\d+).*")
-        for line in lines:
-            # parts = line_re.match(line).groups()
-            parts = re.split(r"-, ", line)
-            # a = parts[0].strip().split(" ")
-            # a = list(map(int, re.findall(r"\d+", parts[0])))
-            self.inp.append(parts)
-
-    def recursive_split(self, seps, inp=None):
-        if inp is None:
-            self.inp = self.recursive_split(seps, self.input_text)
-            return self.inp
-        if not seps:
-            return inp
-        if 1:  # should I strip the parts?
-            return [self.recursive_split(seps[1:], x.strip()) for x in inp.split(seps[0])]
-        else:
-            return [self.recursive_split(seps[1:], x) for x in inp.split(seps[0])]
-
-    def find_all_re(self):
-        pattern = re.compile(r"(\d+)")
-        self.inp = pattern.findall(self.input_text)
-
-    def find_all_per_line_re(self):
-        pattern = re.compile(r"(\d+)")
-        self.inp = [pattern.findall(line) for line in self.input_text.splitlines()]
-
-    def process_map(self):
-        lines = self.input_text.splitlines()
-        self.map = dict()
-        for row, line in enumerate(lines):
-            for col, ch in enumerate(line):
-                self.map[(col, row)] = ch
-        self.rows, self.cols = row + 1, col + 1
-
-    def read_ints(self):
-        self.inp = [int(n) for n in re.findall(r"[+-]?\d+", self.input_text)]
-
-    def read_line_groups(self):
-        self.inp = [re.split("\n", g) for g in re.split("\n\n", self.input_text.strip())]
-
-    def read_int_line_groups(self):
-        self.inp = [
-            [int(n) for n in re.split("\n", g)] for g in re.split("\n\n", self.input_text.strip())
-        ]
-
-    def process_int_list(self):
-        lines = self.input_text.splitlines()
-        self.inp = [list(map(int, re.findall(r"-?\d+", line))) for line in lines]
-
-    def process_int_int_dict(self):
-        lines = self.input_text.splitlines()
-        self.inp = {}
-        for line in lines:
-            parts = list(map(int, re.findall(r"-?\d+", line)))
-            self.inp[parts[0]] = parts[1]
-
-    def process_int_list_dict(self):
-        lines = self.input_text.splitlines()
-        self.inp = {}
-        for line in lines:
-            parts = list(map(int, re.findall(r"-?\d+", line)))
-            self.inp[parts[0]] = parts[1:]
-
-    def print_with_color(self, text, text_color="#FFFFFF", back_color="#000000", end="\n"):
-        from colored import attr, bg, fg
-
-        color = fg(text_color)
-        back = bg(back_color)
-        reset = attr("reset")
-        print(f"{color + back}" + text + f"{reset}", end=end)
-
-    def solve_some_equation(self):
-        x = Symbol("x")
-        y = Symbol("y")
-        s = solve(x**2 - 1, x)
-
-    # region Search Methods
-
-    # @override
-    # def get_next_states(self, state):
-    #     next_states = []
-    #     # state = State(1, 2)
-    #     # next_states.append(state)
-    #     return next_states
-
-    # @override
-    # def is_goal(self, state):
-    #     pass
-
-    # @override
-    # def cost(self, state):
-    #     pass
-
-    # @override
-    # def heuristic(self, state):
-    #     pass
-
-    # @override
-    # def get_result(self, state):
-    #     pass
-
-    # @override
-    # def state_core(self, state):
-    #     """
-    #     Core presentation of the state, in an immutable form.
-    #     Used to avoid repetition of the same state in the search.
-
-    #     Args:
-    #         state: The state to be processed.
-
-    #     Returns:
-    #         The immutable core state.
-    #     """
-    #     return state
-
-    # endregion
+    def calc0(self):
+        # s = MySearch()
+        # s.search(initial_state=State(0, 0))
+        # x = Symbol("x")
+        # y = Symbol("y")
+        # s = solve(x**2 - 1, x)
+        pass
 
     def calc1(self):
         return None
@@ -189,6 +72,7 @@ def test_samples(filename, answer1, answer2):
 if __name__ == "__main__":
     data = get_input_data(__file__)
 
+    # region Tests
     # assert Puzzle("sample1.txt").calc1() == 0
     # assert Puzzle("sample2.txt").calc1() == 0
     # assert Puzzle("sample3.txt").calc1() == 0
@@ -200,6 +84,7 @@ if __name__ == "__main__":
     # assert Puzzle("sample3.txt").calc2() == 0
     # assert Puzzle("sample4.txt").calc2() == 0
     # assert Puzzle("sample5.txt").calc2() == 0
+    # endregion
 
     test_samples("sample1.txt", None, None)
     test_samples("sample2.txt", None, None)
--- python/myutils/utils.py
+++ python/myutils/utils.py
@@ -1,9 +1,138 @@
 import operator
+import re
+from collections import namedtuple
 from functools import reduce
+
+from myutils.search import Search, Search_AStar, Search_BFS, Search_DFS, Search_MinHeap
+
+State = namedtuple("state", ["x", "y"])
+
+
+class MySearch(Search):
+    def get_next_states(self, state):
+        next_states = []
+        # state = State(1, 2)
+        # next_states.append(state)
+        return next_states
+
+    def is_goal(self, state):
+        pass
+
+    def cost(self, state):
+        pass
+
+    def heuristic(self, state):
+        pass
+
+    def get_result(self, state):
+        pass
+
+    def state_core(self, state):
+        """
+        Core presentation of the state, in an immutable form.
+        Used to avoid repetition of the same state in the search.
+
+        Args:
+            state: The state to be processed.
+
+        Returns:
+            The immutable core state.
+        """
+        return state
 
 
 def multiply(elements):
     return reduce(operator.mul, elements, 1)
+
+
+def process(self):
+    lines = self.input_text.splitlines()
+    self.inp = []
+
+    # self.inp = [tuple(map(int, re.split(r"\D", line))) for line in self.lines]
+
+    # line_re = re.compile(r"(\d+)\s*(\d+):(\d+).*")
+    for line in lines:
+        # parts = line_re.match(line).groups()
+        parts = re.split(r"-, ", line)
+        # a = parts[0].strip().split(" ")
+        # a = list(map(int, re.findall(r"\d+", parts[0])))
+        self.inp.append(parts)
+
+
+def recursive_split(self, seps, inp=None, strip=True):
+    if inp is None:
+        self.inp = recursive_split(self, seps, self.input_text)
+        return
+    if not seps:
+        return inp
+    return [
+        recursive_split(self, seps[1:], x.strip() if strip else x, strip)
+        for x in inp.split(seps[0])
+    ]
+
+
+def find_all_re(self):
+    pattern = re.compile(r"(\d+)")
+    self.inp = pattern.findall(self.input_text)
+
+
+def find_all_per_line_re(self):
+    pattern = re.compile(r"(\d+)")
+    self.inp = [pattern.findall(line) for line in self.input_text.splitlines()]
+
+
+def process_map(self):
+    lines = self.input_text.splitlines()
+    self.map = dict()
+    for row, line in enumerate(lines):
+        for col, ch in enumerate(line):
+            self.map[(col, row)] = ch
+    self.rows, self.cols = row + 1, col + 1
+
+
+def read_ints(self):
+    self.inp = [int(n) for n in re.findall(r"[+-]?\d+", self.input_text)]
+
+
+def read_line_groups(self):
+    self.inp = [re.split("\n", g) for g in re.split("\n\n", self.input_text.strip())]
+
+
+def read_int_line_groups(self):
+    self.inp = [
+        [int(n) for n in re.split("\n", g)] for g in re.split("\n\n", self.input_text.strip())
+    ]
+
+
+def process_int_list(self):
+    lines = self.input_text.splitlines()
+    self.inp = [list(map(int, re.findall(r"-?\d+", line))) for line in lines]
+
+
+def process_int_int_dict(self):
+    lines = self.input_text.splitlines()
+    self.inp = {}
+    for line in lines:
+        parts = list(map(int, re.findall(r"-?\d+", line)))
+        self.inp[parts[0]] = parts[1]
+
+
+def process_int_list_dict(self):
+    lines = self.input_text.splitlines()
+    self.inp = {}
+    for line in lines:
+        parts = list(map(int, re.findall(r"-?\d+", line)))
+        self.inp[parts[0]] = parts[1:]
+
+
+def print_with_color(self, text, text_color="#FFFFFF", back_color="#000000", end="\n"):
+    from colored import attr, bg, fg
+
+    color = fg(text_color)
+    back = bg(back_color)
+    reset = attr("reset")
+    print(f"{color + back}" + text + f"{reset}", end=end)
 
 
 # Example usage
