f8d77923f75c6a6c5d1c585296681596907526e1
Amir Shahroudy <amir.shahroudy@zenseact.com>
2025-03-23 00:41:46 +0100

    add i18n day09

---
+++ python/i18n/day09/d09.py
@@ -0,0 +1,38 @@
+import re
+from collections import defaultdict
+from datetime import datetime
+from itertools import permutations
+from pathlib import Path
+
+
+class NineEleven:
+    def __init__(self, filename):
+        self.dates = defaultdict(list)
+        for line in Path(filename).read_text().splitlines():
+            for name in re.findall(r"[a-zA-Z]+", line):
+                self.dates[name].append(list(map(int, re.findall(r"\d+", line))))
+
+    def calc(self):
+        sus_names = set()
+        for name, dates in self.dates.items():
+            for format in permutations(["day", "month", "year"]):
+                try:
+                    sus = False
+                    for date in dates:
+                        kwargs = dict(zip(format, date))
+                        kwargs["year"] += 2000 if kwargs["year"] < 20 else 1900
+                        if datetime(**kwargs) == datetime(2001, 9, 11):
+                            sus = True
+                    if sus:
+                        sus_names.add(name)
+                        break
+                except ValueError:
+                    continue
+        return " ".join(sorted(sus_names))
+
+
+if __name__ == "__main__":
+
+    assert NineEleven("test-input").calc() == "Margot Peter"
+    print("Tests passed, starting with the puzzle")
+    print(NineEleven("input").calc())
