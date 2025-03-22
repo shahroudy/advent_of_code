91c000d744f3680dec68281ec66ab63e68d77d8f
Amir Shahroudy <amir.shahroudy@zenseact.com>
2025-03-23 00:42:00 +0100

    add i18n day10

---
+++ python/i18n/day10/d10.py
@@ -0,0 +1,41 @@
+import concurrent.futures
+from functools import cache
+from pathlib import Path
+from unicodedata import normalize
+
+import bcrypt
+
+
+class UnicodePasswordsStrikeBack:
+    def __init__(self, filename):
+        encrypted_passwords, self.attempts = Path(filename).read_text().split("\n\n")
+        self.passwords = {u: p for u, p in map(str.split, encrypted_passwords.splitlines())}
+
+    @cache
+    def accept(self, user, password):
+        passwords = [""]
+        for ch in password:
+            if ord(ch) < 128:
+                passwords = [p + ch for p in passwords]
+            else:
+                passwords = [p + ch for p in passwords] + [
+                    p + normalize("NFD", ch) for p in passwords
+                ]
+        return any(bcrypt.checkpw(p.encode(), self.passwords[user].encode()) for p in passwords)
+
+    def accept_line(self, line):
+        user, password = line.split()
+        return self.accept(user, normalize("NFC", password))
+
+    def no_of_valid_attempts(self):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(self.accept_line, line) for line in self.attempts.splitlines()
+            ]
+        return sum(future.result() for future in futures)
+
+
+if __name__ == "__main__":
+    assert UnicodePasswordsStrikeBack("test-input").no_of_valid_attempts() == 4
+    print("Tests passed, starting with the puzzle")
+    print(UnicodePasswordsStrikeBack("input").no_of_valid_attempts())
